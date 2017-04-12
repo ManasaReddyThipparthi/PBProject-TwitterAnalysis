271b40c57ef31b7285af742108d5c59ad53f59ec
ManasaReddyThipparthi <manasareddy.thipparthi@gmail.com>
2017-04-12 18:43:26 -0500

    commit

--- Project-2/Source/bestTimetoTweet.py
+++ Project-2/Source/bestTimetoTweet.py
@@ -13,12 +13,12 @@ def calculateBestTime(file):
             createdAt=json.loads(line)['created_at']
             date= datetime.strptime(createdAt, "%a %b %d %H:%M:%S +0000 %Y")
             hour = date.hour
-            if date.minute in data.keys():
-                data[date.minute]+=1;
+            if date.hour in data.keys():
+                data[date.hour]+=1;
             else:
-                data[date.minute]=1;  
+                data[date.hour]=1;  
     sortedData=sorted(data.items(), key=operator.itemgetter(1),reverse=True)
-    print("Best time to Tweet : ",hour,":",sortedData[0][0],"-",hour,":",sortedData[0][0]+1)
+    print(sortedData[0][0])
 def main():
     tweet_file = open(sys.argv[1])
     calculateBestTime(sys.argv[1])
